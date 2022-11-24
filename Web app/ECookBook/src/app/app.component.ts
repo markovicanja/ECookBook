cccbd65f54caa6de0d58a258f0c8aa7365c32577
Anja Markovic <markovicanja98@gmail.com>
2022-11-24 23:31:55 +0100

    Fixed navbar router links

--- Web app/ECookBook/src/app/app.component.ts
+++ Web app/ECookBook/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './model/user.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ECookBook';
+
+  constructor(private router: Router) {
+  }
+
+  sessionValid: boolean;
+  loggedUser: User | null;
+
+  ngOnInit(): void {
+    if (localStorage.getItem("user") == null) {
+      this.sessionValid = false;
+      this.loggedUser = null;
+    }
+    else {
+      this.sessionValid = true;
+      this.loggedUser = JSON.parse(localStorage.getItem("user")!);
+    }
+  }  
+
+  logout() {
+    localStorage.removeItem("user");
+    this.loggedUser = null;
+    this.sessionValid = false;
+    this.router.navigate(["home"]);
+  }
 }
