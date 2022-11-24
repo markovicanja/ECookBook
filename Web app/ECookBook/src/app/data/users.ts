fd8494f66a085696fa2ca5e9a2552ee3a3fdaff0
Anja Markovic <markovicanja98@gmail.com>
2022-11-24 22:41:49 +0100

    Login

---
+++ Web app/ECookBook/src/app/data/users.ts
@@ -0,0 +1,19 @@
+import {User} from '../model/user.model';
+
+export var allUsers: User[] = [
+    {
+        username: "anja", 
+        email: "anja@gmail.com",
+        password: "123", 
+    },
+    {
+        username: "ogi", 
+        email: "ogi@gmail.com",
+        password: "123", 
+    },
+    {
+        username: "nina", 
+        email: "nina@gmail.com",
+        password: "123", 
+    } 
+]
--- Web app/ECookBook/src/app/home/home.component.ts
+++ Web app/ECookBook/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { allUsers } from '../data/users';
+import { User } from '../model/user.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  username: string;
+  password: string;
+  errorMsg: string;
+  allUsers: User[];
 
   ngOnInit(): void {
+    if (JSON.parse(localStorage.getItem("user")!) != null) {
+      this.router.navigate(["home"]);
+    }
+    if (localStorage.getItem("allUsers") == "" || localStorage.getItem("allUsers") == null) {
+      this.allUsers = allUsers;
+      localStorage.setItem("allUsers", JSON.stringify(this.allUsers)); 
+    }
+    else {
+      this.allUsers = JSON.parse(localStorage.getItem("allUsers")!);
+    }    
+    this.username = "";
+    this.password = "";
+    this.errorMsg = "";
   }
+
+  reset() {
+    this.username = "";
+    this.password = "";
+    this.errorMsg = "";
+  }
+
+  login() {
+    this.errorMsg = "";
+    if (this.username == "" || this.password == "") {
+      this.errorMsg = "Morate popuniti sva polja.";
+      return;
+    }
+    let user = this.allUsers.find(user => user.username == this.username && user.password == this.password);
+    if (user == null) {
+      this.errorMsg = "Neispravni kredencijali.";
+      return;
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    window.location.reload();
+  }
+
 
 }
