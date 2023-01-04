47dbcdd7927ac531f7bcd3ffc8c9af5e34b7893d
giska1923 <37376562+giska1923@users.noreply.github.com>
2023-01-04 22:26:22 +0100

    Recipe search and sort logics

--- Web app/ECookBook/src/app/recipes/recipes.component.ts
+++ Web app/ECookBook/src/app/recipes/recipes.component.ts
@@ -14,10 +14,21 @@ export class RecipesComponent implements OnInit {
 
   allRecipes: Recipe[];
 
+  queriedRecipes: Recipe[];
+  searchQuery: string;
+
+  sorter: string;
+
+  selectedCategory: string;
+
   ngOnInit(): void {
+    this.searchQuery = "";
+    this.sorter = "Sort";
+    this.selectedCategory = "All";
     this.service.getAllRecipes().subscribe(res => {
       if(res["status"] == 1){
         this.allRecipes = res["poruka"];
+        this.queriedRecipes = this.allRecipes;
       }
     });
   }
@@ -27,8 +38,42 @@ export class RecipesComponent implements OnInit {
     this.router.navigate(["recipe"]);
   }
 
-  search() {
-    
+  search(){
+    const stringToDetect = this.searchQuery.toLowerCase();
+    this.queriedRecipes = this.allRecipes.filter(recipe => recipe.name.toLowerCase().includes(stringToDetect));
+
+    this.sort();
   }
 
+  sort(){
+    this.sortByCat();
+    if(this.sorter !== "Sort") this.sortByNum();
+  }
+
+  sortByCat(){
+    if(this.selectedCategory !== "All"){
+      this.queriedRecipes = this.queriedRecipes.filter(recipe => recipe.category === this.selectedCategory);
+    }
+  }
+  
+  sortByNum(){
+    if(this.sorter === "sort-diff-asc") this.queriedRecipes.sort((a, b) => a.difficulty - b.difficulty);
+    if(this.sorter === "sort-diff-desc") this.queriedRecipes.sort((a, b) => b.difficulty - a.difficulty);
+    if(this.sorter === "sort-rtg-asc") this.queriedRecipes.sort((a, b) => a.rating - b.rating);
+    if(this.sorter === "sort-rtg-desc") this.queriedRecipes.sort((a, b) => b.rating - a.rating);
+  }
+  
+  categorySelected(newCategory: string){
+    this.selectedCategory = newCategory;
+
+    this.search();
+  }
+
+  onKeyUp(x : Event) {
+    this.search();
+  }
+
+  onOptionSelect(x : Event) {
+    this.sort();
+  }
 }
